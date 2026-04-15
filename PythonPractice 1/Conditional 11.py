4744148b791ca93c35a8013c944635286f2d7f8b
SubashiniArun19 <subashinia2005@gmail.com>
2026-04-15 16:34:44 +0530

    Delete PythonPractice 1/Conditional 11.py

--- PythonPractice 1/Conditional 11.py
+++
@@ -1,7 +0,0 @@
-age=int(input("Enter your age:"))
-if age>=18:
-    print("You are eligible to vote")
-elif age<18 and age>0:
-    print("You are not eligible to vote")
-else:
-    print("Valid number")
