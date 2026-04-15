a7e78b7097348e9f5631541411d91554832aa21f
SubashiniArun19 <subashinia2005@gmail.com>
2026-04-15 16:34:55 +0530

    Delete PythonPractice 1/Conditional 12.py

--- PythonPractice 1/Conditional 12.py
+++
@@ -1,9 +0,0 @@
-mark=int(input("Enter mark: "))
-if mark>=90:
-    print(" A Grade")
-elif mark<=89 and mark>=70:
-    print(" B Grade")
-elif mark<=69 and mark>=50:
-    print("C Grade")
-else:
-    print("Fail")
